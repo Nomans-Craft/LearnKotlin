4d0e8238af0586818a826cea4bd7a39a3b3f8ff0
Noman-Win <mr.noman1971@gmail.com>
2023-06-04 03:31:05 +0600

    Hash codes and equality

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -3,16 +3,35 @@
 * A Set can't hold duplicate value.
 * Set is an unordered collection.
 * Use the contains function to check whether a Set contains a particular value.
+* A Set use hash code to store its elements.
+* The Set use "===" operator to compare the new value.
+* Equal objects must have matching has codes.
 * */
 
 fun main() {
-    println("How to use a Set's value")
+    println("Hash codes and equality")
 
-    val friendSet: Set<String> = setOf("Jim", "Sue", "Nick", "Sue")
-    println("$friendSet")
+    println("Equality using the === operator")
+    val a = "Sue"
+    val b = a
+    val setA = setOf(a, b)
+    println(setA)
 
-    val isFredGoing = friendSet.contains("Fred")
-    println(isFredGoing)
+    println("Equality using the == operator")
+    val c = Recipe("Thai Curray")
+    val d = Recipe("Thai Curry")
+    val setB = setOf(c, d)
+    println(setB)
 
-    for (item in friendSet) println(item)
+    println("hasCode of a is: ${a.hashCode()}")
+    println("hasCode of b is: ${b.hashCode()}")
+    println("hasCode of c is: ${c.hashCode()}")
+    println("hasCode of d is: ${d.hashCode()}")
+    println("hasCode of setA is: ${setA.hashCode()}")
+    println("hasCode of setB is: ${setB.hashCode()}")
+}
+class Recipe(name: String) {
+    fun message() {
+        println("Today recipe is $this")
+    }
 }
