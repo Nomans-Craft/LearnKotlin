7426bcc56b5f5f751df7a8271118a49df5091a77
Noman-Win <mr.noman1971@gmail.com>
2023-06-04 03:08:38 +0600

    How to use a Set's value

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -1,13 +1,18 @@
 /* *
+* A Set is immutable.
 * A Set can't hold duplicate value.
 * Set is an unordered collection.
+* Use the contains function to check whether a Set contains a particular value.
 * */
 
-fun main(args: Array<String>) {
-    println("How to create a Set")
+fun main() {
+    println("How to use a Set's value")
 
     val friendSet: Set<String> = setOf("Jim", "Sue", "Nick", "Sue")
     println("$friendSet")
 
-    for (i in friendSet) println(i)
+    val isFredGoing = friendSet.contains("Fred")
+    println(isFredGoing)
+
+    for (item in friendSet) println(item)
 }
