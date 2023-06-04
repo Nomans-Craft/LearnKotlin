065e305d8b10642a64e902cd321204a1c8796b3c
Noman-Win <mr.noman1971@gmail.com>
2023-06-04 17:40:46 +0600

    How to use a Map

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -5,4 +5,20 @@ fun main() {
 
     val recipeMap = mapOf("Recipe1" to r1, "Recipe2" to r2, "Recipe3" to r3)
     println(recipeMap)
+
+    println(recipeMap.containsKey("Recipe1"))
+
+    val recipeToCheck = "Chicken Soup"
+    if (recipeMap.containsValue(recipeToCheck)) {
+        println("recipeMap contains a recipe for Chicken Soup.")
+    }
+
+    if (recipeMap.containsKey("Recipe1")) {
+        val recipe = recipeMap.getValue("Recipe1")
+        println(recipe)
+    }
+
+    for ((key, value) in recipeMap) {
+        println("Key is $key, value is $value")
+    }
 }
