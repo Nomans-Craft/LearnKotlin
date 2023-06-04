469807e8e43facef956581bcb83f4503cf874649
Noman-Win <mr.noman1971@gmail.com>
2023-06-04 19:25:58 +0600

    Mixed messages (Collection)

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -1,39 +1,31 @@
 /**
- * POOL PUZZLE
- * Your job is to take code snippets from the pool and place them into the
- * blank lines in the code. You may not use the same code snippet more
- * than once, and you won’t need to use all the code snippets. Your goal is
- * to print the entries of a Map named glossary that provides definitions of
- * all the collection types you’ve learned about.
- * Code snippets in below:
- * "MutableArray", "MutableList", mapOf, Map, term1, term2, term3, term4, term5, term6, term7
+ * MIXED MESSAGES
+ * A short Kotlin program is listed below. One block of the program is
+ * missing. Your challenge is to match the candidate block of code (on the
+ * left), with the output that you’d see if the block were inserted. Not all the
+ * lines of output will be used, and some lines of output may be used more
+ * than once. Draw lines connecting the candidate blocks of code with their
+ * matching output.
  */
 fun main() {
-    val term1 = "Array"
-    val term2 = "List"
-    val term3 = "Map"
-    val term4 = "MUtableList" //Your code here
-    val term5 = "MutableMap"
-    val term6 = "MutableSet"
-    val term7 = "Set"
+    val mList = mutableListOf("Football", "Baseball", "basketball")
 
-    val def1 = "Hold Values in no particular order."
-    val def2 = "Holds key/value pairs."
-    val def3 = "Holds values in a sequence."
-    val def4 = "Can be updated."
-    val def5 = "Can't be updated."
-    val def6 = "Can be resized."
-    val def7 = "Can't be resized."
+    mList.sort()
+    println(mList)
 
-    val glossary = mapOf(
-        term4 to "$def3 $def4 $def6", //Your code here
-        term7 to "$def1 $def5 $def7", //Your code here
-        term1 to "$def3 $def4 $def7", //Your code here
-        term5 to "$def2 $def4 $def6", //Your code here
-        term2 to "$def3 $def5 $def7", //Your code here
-        term6 to "$def1 $def4 $def6", //Your code here
-        term3 to "$def2 $def5 $def7", //Your code here
-    )
+    val mMap = mutableMapOf("0" to "Netball")
+    var x = 0
+    for (item in mList) {
+        mMap.put(x.toString(), item)
+    }
+    println(mMap.values)
 
-    for ((key, value) in glossary) println("$key: $value")
+    mList.addAll(mList)
+    mList.reverse()
+    val set = mList.toSet()
+    println(set)
+
+    mList.sort()
+    mList.reverse()
+    println(mList)
 }
