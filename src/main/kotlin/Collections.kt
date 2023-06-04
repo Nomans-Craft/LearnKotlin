f7fb10d19a31a4426e83a7df09c65951fca9e23a
Noman-Win <mr.noman1971@gmail.com>
2023-06-04 17:48:41 +0600

    Create a MutableMap

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -1,24 +1,7 @@
 fun main() {
     val r1 = "Chicken Soup"
     val r2 = "Quinoa Salad"
-    val r3 = "Thai Curry"
 
-    val recipeMap = mapOf("Recipe1" to r1, "Recipe2" to r2, "Recipe3" to r3)
-    println(recipeMap)
-
-    println(recipeMap.containsKey("Recipe1"))
-
-    val recipeToCheck = "Chicken Soup"
-    if (recipeMap.containsValue(recipeToCheck)) {
-        println("recipeMap contains a recipe for Chicken Soup.")
-    }
-
-    if (recipeMap.containsKey("Recipe1")) {
-        val recipe = recipeMap.getValue("Recipe1")
-        println(recipe)
-    }
-
-    for ((key, value) in recipeMap) {
-        println("Key is $key, value is $value")
-    }
+    val mRecipeMap = mutableMapOf("Recipe1" to r1, "Recipe2" to r2)
+    println(mRecipeMap)
 }
