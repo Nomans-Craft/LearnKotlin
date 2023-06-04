478f5a360d7f2c8d45ed0282462c6fb82c9e4877
Noman-Win <mr.noman1971@gmail.com>
2023-06-04 13:37:17 +0600

    How to use a MutableSet

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -1,37 +1,24 @@
-/* *
-* A Set is immutable.
-* A Set can't hold duplicate value.
-* Set is an unordered collection.
-* Use the contains function to check whether a Set contains a particular value.
-* A Set use hash code to store its elements.
-* The Set use "===" operator to compare the new value.
-* Equal objects must have matching has codes.
-* */
-
 fun main() {
-    println("Hash codes and equality")
+    println("How to use a MutableSet")
 
-    println("Equality using the === operator")
-    val a = "Sue"
-    val b = a
-    val setA = setOf(a, b)
-    println(setA)
+    val mFriendSet = mutableSetOf("Jim", "Sue")
+    mFriendSet.add("Nick")
+    println("${mFriendSet.add("Sue")}")//Duplicate value return "false"
+    mFriendSet.remove("Jim")
+    println("$mFriendSet")
 
-    println("Equality using the == operator")
-    val c = Recipe("Thai Curray")
-    val d = Recipe("Thai Curry")
-    val setB = setOf(c, d)
-    println(setB)
+    val toAdd = setOf("Joe", "Mia")
+    mFriendSet.addAll(toAdd)
+    println(mFriendSet)
 
-    println("hasCode of a is: ${a.hashCode()}")
-    println("hasCode of b is: ${b.hashCode()}")
-    println("hasCode of c is: ${c.hashCode()}")
-    println("hasCode of d is: ${d.hashCode()}")
-    println("hasCode of setA is: ${setA.hashCode()}")
-    println("hasCode of setB is: ${setB.hashCode()}")
-}
-class Recipe(name: String) {
-    fun message() {
-        println("Today recipe is $this")
-    }
+    val toRemove = setOf("Mia")
+    mFriendSet.removeAll(toRemove)
+    println(mFriendSet)
+
+    val toRetain = setOf("Sue", "Nick")
+    mFriendSet.retainAll(toRetain)
+    println(mFriendSet)
+
+    mFriendSet.clear()
+    println(mFriendSet)
 }
