48191f29d6b61adaf49825685b43ffba4357de99
Noman-Win <mr.noman1971@gmail.com>
2023-06-04 20:33:55 +0600

    Generic function

--- src/main/kotlin/Generics.kt
+++ src/main/kotlin/Generics.kt
@@ -6,6 +6,26 @@ fun main() {
     class Fish(name: String) : Pet(name)
 
     class Contest<T: Pet> {
+        val scores: MutableMap<T, Int> = mutableMapOf()
 
+        fun addScores(t: T, score: Int = 0) {
+            if (score >= 0) scores.put(t, score)
+        }
+
+        fun getWinners(): MutableSet<T> {
+            val highScore = scores.values.max()
+            val winners : MutableSet<T> = mutableSetOf()
+            for ((t, score) in scores) {
+                if (score == highScore) winners.add(t)
+            }
+            return winners
+        }
     }
+
+    val catContest = Contest<Cat>()
+    catContest.addScores(Cat("Fuzz Lightyear"), 50)
+    catContest.addScores(Cat("Katsu"), 45)
+    val topCat = catContest.getWinners().first()
+
+    println(topCat.name)
 }
