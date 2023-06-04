363d1ebd9b2280b1475ad2b53f729a1702d6a096
Noman-Win <mr.noman1971@gmail.com>
2023-06-04 17:28:53 +0600

    Create a Map

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -1,30 +1,8 @@
-/*
-* EXERCISE: SHARPEN YOUR PENCIL
-* Four friends have each made a List of their pets. One item in the List
-* represents one pet. Here are the four lists.
-* val petsLiam = listOf("Cat", "Dog", "Fish", "Fish")
-* val petsSophia = listOf("Cat", "Owl")
-* val petsNoah = listOf("Dog", "Dove", "Dog", "Dove")
-* val petsEmily = listOf("Hedgehog")
-* Write the code to print how many types of pet there are.
- */
 fun main() {
-    val petsLiam = listOf("Cat", "Dog", "Fish", "Fish")
-    val petsSophia = listOf("Cat", "Owl")
-    val petsNoah = listOf("Dog", "Dove", "Dog", "Dove")
-    val petsEmily = listOf("Hedgehog")
+    val r1 = "Chicken Soup"
+    val r2 = "Quinoa Salad"
+    val r3 = "Thai Curry"
 
-    val pets: MutableList<String> = mutableListOf()
-    pets.addAll(petsLiam)
-    pets.addAll(petsSophia)
-    pets.addAll(petsNoah)
-    pets.addAll(petsEmily)
-    pets.size
-
-    val petSet = pets.toMutableSet()
-    println(petSet.size)
-
-    val petList = petSet.toMutableList()
-    petList.sort()
-    println(petList)
+    val recipeMap = mapOf("Recipe1" to r1, "Recipe2" to r2, "Recipe3" to r3)
+    println(recipeMap)
 }
