687b79475d3de2e77ba0fc4dd73d77c88d74036f
Noman <mr.noman1971@gmail.com>
2023-06-05 01:28:39 +0600

    Generic Interface

--- src/main/kotlin/Generics.kt
+++ src/main/kotlin/Generics.kt
@@ -1,27 +1,22 @@
-/**
- * Your job is to take code snippets from the pool and place them into the
- * blank lines in the code. You may not use the same code snippet more
- * than once, and you won’t need to use all the code snippets. Your goal is
- * to create a class named PetOwner that accepts generic Pet types, which
- * you must then use to create a new PetOwner<Cat> that holds references
- * to two Cat objects.
- */
+interface Retailer<T> {
+    fun sell(): T
+}
 
-class PetOwner<T: Pet>(t:T) {
-    val pets = mutableListOf(t)
-
-    fun add(t: T) {
-        pets.add(t)
-    }
-
-    fun remove(t: T) {
-        pets.remove(t)
+class CatRetailer : Retailer<Cat> {
+    override fun sell(): Cat {
+        println("Sell cat")
+        return Cat("")
     }
 }
+
+class DogRetailer : Retailer<Dog> {
+    override fun sell(): Dog {
+        println("Sell dog")
+        return Dog("")
+    }
+}
+
 fun main() {
-    val catFuzz = Cat("Fuzz Lightyear")
-    val catKatsu = Cat("Katsu")
-    val fishFinny = Cat("Finny McGraw")
-    val catOwner = PetOwner(catFuzz)
-    catOwner.add(catKatsu)
+    val catRetailer1 = CatRetailer()
+    val catRetailer2 : CatRetailer = CatRetailer()
 }
