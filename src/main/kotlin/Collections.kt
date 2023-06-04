85d1fcdc033c4910e5e1ab7105ef8b045076d491
Noman-Win <mr.noman1971@gmail.com>
2023-06-04 19:14:02 +0600

    Pool puzzle solution (Collection)

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -1,48 +1,39 @@
-data class Recipe (var name: String)
+/**
+ * POOL PUZZLE
+ * Your job is to take code snippets from the pool and place them into the
+ * blank lines in the code. You may not use the same code snippet more
+ * than once, and you won’t need to use all the code snippets. Your goal is
+ * to print the entries of a Map named glossary that provides definitions of
+ * all the collection types you’ve learned about.
+ * Code snippets in below:
+ * "MutableArray", "MutableList", mapOf, Map, term1, term2, term3, term4, term5, term6, term7
+ */
 fun main() {
-    println("Collection project")
+    val term1 = "Array"
+    val term2 = "List"
+    val term3 = "Map"
+    val term4 = "MUtableList" //Your code here
+    val term5 = "MutableMap"
+    val term6 = "MutableSet"
+    val term7 = "Set"
 
-    var mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
-    println("mShoppingList original: $mShoppingList")
+    val def1 = "Hold Values in no particular order."
+    val def2 = "Holds key/value pairs."
+    val def3 = "Holds values in a sequence."
+    val def4 = "Can be updated."
+    val def5 = "Can't be updated."
+    val def6 = "Can be resized."
+    val def7 = "Can't be resized."
 
-    val extraShopping= listOf("Cookies", "Sugar", "Eggs")
-    mShoppingList.addAll(extraShopping)
-    println("mShoppingList items added: $mShoppingList")
+    val glossary = mapOf(
+        term4 to "$def3 $def4 $def6", //Your code here
+        term7 to "$def1 $def5 $def7", //Your code here
+        term1 to "$def3 $def4 $def7", //Your code here
+        term5 to "$def2 $def4 $def6", //Your code here
+        term2 to "$def3 $def5 $def7", //Your code here
+        term6 to "$def1 $def4 $def6", //Your code here
+        term3 to "$def2 $def5 $def7", //Your code here
+    )
 
-    if (mShoppingList.contains("Tea")) {
-        mShoppingList.set(mShoppingList.indexOf("Tea"), "Cofffee")
-    }
-
-    mShoppingList.sort()
-    println("mShoppingList sorted: $mShoppingList")
-
-    mShoppingList.reverse()
-    println("mShoppingList reversed: $mShoppingList")
-
-    val mShoppingSet = mShoppingList.toMutableSet()
-    println("mShoppingSet: $mShoppingSet")
-
-    val moreShopping = setOf("Chives", "Spinach", "Milk")
-    mShoppingSet.addAll(moreShopping)
-    println("mShoppingSet items added: $mShoppingSet")
-
-    mShoppingList = mShoppingSet.toMutableList()
-    println("mShoppingList new version: $mShoppingList")
-
-    val r1 = Recipe("Chicken Soup")
-    val r2 = Recipe("Quinoa Salad")
-    val r3 = Recipe("Thai Curry")
-    val r4 = Recipe("Jambalaya")
-    val r5 = Recipe("Sausage Rolls")
-
-    val mRecipeMap = mutableMapOf("Recipe1" to r1, "Recipe2" to r2, "Recipe3" to r3)
-    println("mRecipeMap original: $mRecipeMap")
-
-    val recipesToAdd = mapOf("Recipe4" to r4, "Recipe5" to r5)
-    mRecipeMap.putAll(recipesToAdd)
-    println("mRecipeMap updated: $mRecipeMap")
-
-    if (mRecipeMap.containsKey("Recipe1")) {
-        println("Recipe1 is: ${mRecipeMap.getValue("Recipe1")}")
-    }
+    for ((key, value) in glossary) println("$key: $value")
 }
