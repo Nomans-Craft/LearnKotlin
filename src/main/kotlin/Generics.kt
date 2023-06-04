16c62bc7bd2d4fe06298dcec508e0b2d70c6959b
Noman-Win <mr.noman1971@gmail.com>
2023-06-04 19:58:35 +0600

    Create class hierarchy

---
+++ src/main/kotlin/Generics.kt
@@ -0,0 +1,7 @@
+fun main() {
+    abstract class Pet(var name: String)
+
+    class Cat(name: String) : Pet(name)
+    class Dog(name: String) : Pet(name)
+    class Fish(name: String) : Pet(name)
+}
