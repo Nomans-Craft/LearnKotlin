31b08aa810dd02a389503dcb6e26fb1a16d932ae
Noman <mr.noman1971@gmail.com>
2023-06-12 14:11:34 +0600

    Rewind: Class Inheritance

--- src/main/kotlin/Class.kt
+++ src/main/kotlin/Class.kt
@@ -1,3 +1,21 @@
-fun main() {
+open class Person(val name: String, var age: Int) {
+    fun sayHello() {
+        println("Hello, my name is $name!")
+    }
+}
 
+class Student(name: String, age: Int, val studentId: Int): Person(name, age) {
+    fun study() {
+        println("$name is studying!")
+    }
+}
+
+fun main() {
+    val person = Person("Noman", 35)
+    println(person.name)
+    person.sayHello()
+    val student = Student("Lenin", 36, 1001)
+    println(student.studentId)
+    student.sayHello()
+    student.study()
 }
