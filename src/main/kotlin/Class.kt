940cb630c8cccbddb681fe78b4039032ac42853b
Noman <mr.noman1971@gmail.com>
2023-06-12 13:43:11 +0600

    Rewind: Class

--- src/main/kotlin/Class.kt
+++ src/main/kotlin/Class.kt
@@ -1,3 +1,11 @@
-fun main() {
+class Person(val name: String, var age: Int) {
+    fun sayHello() {
+        println("Hello, my name is $name!")
+    }
+}
 
+fun main() {
+    val person = Person("Alice", 25)
+    println(person.name)
+    person.sayHello()
 }
