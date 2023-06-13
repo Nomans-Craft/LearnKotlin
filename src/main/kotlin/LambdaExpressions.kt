f7c961b41c9927308326b4d0d1a2181e1bf7bca6
Noman <mr.noman1971@gmail.com>
2023-06-13 14:31:49 +0600

    Pool puzzle solution

--- src/main/kotlin/LambdaExpressions.kt
+++ src/main/kotlin/LambdaExpressions.kt
@@ -1,37 +1,16 @@
-fun main() {
-    val addFive = {x: Int -> x + 5}
-    var addSix = {x: Int -> x + 6}
-    addSix = {x: Int -> x + 66}
-    fun addFive(x: Int) = x + 5
-    {x: Int, y: Int -> x + y}
-    {"Pow!"}
+fun unless(condition: Boolean, code: () -> Unit ) {
+    if (!condition) {
+        code()
+    }
+}
 
-    val addInts = {x: Int, y: Int -> x + y}
-    val result = addInts.invoke(6, 7)
-
-    println(result)
-    println(addFive.invoke(10))
-
-    println("Lambda's type")
-    //(parameters) -> return_type
-    val msg = {x: Int -> "The value is $x"}
-
-    val add: (Int, Int) -> Int
-    add = {x: Int, y: Int -> x + y}
-    println(add.invoke(9, 9))
-
-    val greeting: () -> String
-    greeting = {"Hello"}
-    println(greeting.invoke())
-
-    val addSeven: (Int) -> Int = {x: Int -> x + 7}
-    val addEight: (Int) -> Int = {x -> x + 8}
-
-    println("Single parameter with it")
-    val addNine: (Int) -> Int = {it + 9}
-
-    val myLambda: () -> Unit = {println("Hi!")}
-    val calculation: (Int, Int) -> Unit = {x, y -> x + y}
-
-
+fun main(args: Array<String>) {
+    val options = arrayOf("Red", "Amber", "Green")
+    var crossWalk = options[(Math.random() * options.size).toInt()]
+    if (crossWalk == "Green") {
+        println("Walk!")
+    }
+    unless(crossWalk == "Green") {
+        println("Stop!")
+    }
 }
