7c22241944b2a7c137548aab40df3a607c6c005b
Noman <mr.noman1971@gmail.com>
2023-06-14 00:48:10 +0600

    Function can return a lambda

--- src/main/kotlin/LambdaExpressions.kt
+++ src/main/kotlin/LambdaExpressions.kt
@@ -1,16 +1,32 @@
-fun unless(condition: Boolean, code: () -> Unit ) {
+/*fun unless(condition: Boolean, code: () -> Unit ) {
     if (!condition) {
         code()
+    }
+}*/
+
+fun getConversionLambda(str: String): (Double) -> Double {
+    if (str == "CentigradeToFahrenheit") {
+        return {it * 1.8 + 32}
+    } else if (str == "KgsToPounds") {
+        return {it * 2.204623}
+    } else if (str == "PoundToUSTons") {
+        return { it / 2000.00 }
+    } else {
+        return {it}
     }
 }
 
 fun main(args: Array<String>) {
-    val options = arrayOf("Red", "Amber", "Green")
+    /*val options = arrayOf("Red", "Amber", "Green")
     var crossWalk = options[(Math.random() * options.size).toInt()]
     if (crossWalk == "Green") {
         println("Walk!")
     }
     unless(crossWalk == "Green") {
         println("Stop!")
-    }
+    }*/
+
+    val pounds = getConversionLambda("KgToPounds") (2.5)
+
+
 }
