01ff47a6213f6b3938e878a13de202336f1f9769
Noman <mr.noman1971@gmail.com>
2023-06-16 15:38:52 +0600

    typealias

--- src/main/kotlin/LambdaExpressions.kt
+++ src/main/kotlin/LambdaExpressions.kt
@@ -1,45 +1,25 @@
-/*fun unless(condition: Boolean, code: () -> Unit ) {
-    if (!condition) {
-        code()
-    }
-}*/
+//Type Alias
 
-/*fun getConversionLambda(str: String): (Double) -> Double {
-    if (str == "CentigradeToFahrenheit") {
-        return {it * 1.8 + 32}
-    } else if (str == "KgsToPounds") {
-        return {it * 2.204623}
-    } else if (str == "PoundToUSTons") {
-        return { it / 2000.00 }
-    } else {
-        return {it}
-    }
-}*/
+typealias DoubleConversion = (Double) -> Double
+typealias DuckArray = Array<Duck>
 
-fun combine(lambda1: (Double) -> Double,
-            lambda2: (Double) -> Double): (Double) -> Double {
+class Duck
+fun converter(x: Double,
+              converter: DoubleConversion): Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+fun combine(lambda1: DoubleConversion,
+            lambda2: DoubleConversion): DoubleConversion {
               return {x: Double -> lambda2(lambda1(x))}
             }
 
 fun main(args: Array<String>) {
-    /*val options = arrayOf("Red", "Amber", "Green")
-    var crossWalk = options[(Math.random() * options.size).toInt()]
-    if (crossWalk == "Green") {
-        println("Walk!")
-    }
-    unless(crossWalk == "Green") {
-        println("Stop!")
-    }*/
-
-    //val pounds = getConversionLambda("KgToPounds") (2.5)
-
-    //Define tow conversion lambdas
     val kgsToPounds = {x: Double -> x * 2.204623}
     val poundsToUsTons = {x: Double -> x / 2000.0}
 
-    //Combine the two lambdas to create a new one
     val kgsToUSTons = combine(kgsToPounds, poundsToUsTons)
 
-    //Invoke the kgsToUSTons lambda
     val usTons = kgsToUSTons(1000.0)
 }
